6426933d170acbf12ec21d6c7479684d4f1829a5
RazvanBuzincu <razvanbuzincu@gmail.com>
2024-02-06 17:07:29 -0600

    second commit

--- car_shop/__init__.py
+++ car_shop/__init__.py
@@ -1,18 +1,36 @@
 from flask import Flask 
-from .blueprints.site.routes import site #add this import to grab out site blueprint
+# ADD THIS
+from flask_migrate import Migrate 
+
+
+#internal imports 
+from .blueprints.site.routes import site 
+from config import Config 
+# ADD THIS
+from .models import login_manager, db 
 
 #instantiating our Flask app
 app = Flask(__name__) #passing in the __name__ variable which just takes the name of the folder we're in
+#add this
+app.config.from_object(Config)
+
+# ADD THIS
+#wrap our app in login_manager so we can use it wherever in our app
+login_manager.init_app(app)
+login_manager.login_view = 'auth.sign_in' 
+login_manager.login_message = "Hey you! Log in please!"
+login_manager.login_message_category = 'warning'
 
 
 #we are going to use a decorator to create our first route
-#@app.route("/")
-#def hello_world():
-#    return "<p>Hello World!</p>"
-
-# We can also comment out the above because this is rendering to the same
-# location as our site blueprint. 
+# @app.route("/")
+# def hello_world():
+#     return "<p>Hello World!</p>"
 
 
-app.register_blueprint(site) # add this here to register your site blueprint
+app.register_blueprint(site)
 
+#ADD THIS
+#instantiating our datbase & wrapping our app
+db.init_app(app)
+migrate = Migrate(app, db)
